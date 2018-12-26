73e5f709c643ba0632b500bde148f8b69ffcdba9
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2018-12-26 14:11:37 +0530

    Add files via upload

---
+++ decisions.js
@@ -0,0 +1,31 @@
+/* var count =3;
+if (count == 4) {
+    console.log('Count is 4');
+} else if (count > 4) {
+    console.log('Count is greter then 4');
+} else if (count< 4) {
+    console.log('count is less then four');
+} else {
+    console.log('count is not Four');
+}
+*/
+/*
+let hero = 'Superman';
+switch (hero.toLocaleLowerCase()) {
+    case 'superman' :
+        console.log('super strength');
+        console.log('X-ray Vision');
+        break;
+    case 'batman':
+        console.log('intelligence');
+        console.log('fighting skills');
+        break;
+    default:
+        console.log('member of JLA');
+}
+*/
+
+// ternary operator
+let a = 1, b = '1';
+let result = (a == b) ? 'equal' : 'inequal';
+console.log(result);
