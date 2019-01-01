b28dddb9e21625e648d6bde8c6fc24d7250a665d
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2019-01-01 19:07:36 +0530

    Add files via upload

---
+++ natives.js
@@ -0,0 +1,15 @@
+let myString = new String('Alok');
+//console.log(myString);
+console.log(myString.toString());
+
+console.log(typeof myString);
+
+/* let myPrimitive = 'THIS IS CRAZY';
+console.log(typeof myPrimitive);
+myPrimitive = myPrimitive.toLowerCase();
+console.log(typeof myPrimitive); */
+
+let myNumber = new Number(7);
+console.log(typeof myNumber);
+let myPrimitive = myNumber.valueOf();
+console.log(typeof myPrimitive);
